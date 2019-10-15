4b29c72675780a18737db689c3551f0567dd901d
unknown <marvinakelly75@gmail.com>
2019-10-15 18:07:06 +0100

    twat

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+import os
+sentence = input('lets talk about something! ' )
+path = ('C:/University/4006CEM(programming Project)/Topics')
+os.chdir(path)
+dir_path = os.path.dirname(path)
+for root ,dirs, files, in os.walk(dir_path):
+    for file in files:
+        if file.endswith('.txt'):
+            #print(file)
+            topic = '{}'.format(file)
+            name = open(file)
+            topic = list((name.read().split(',')))
+counter  = 0
+sentence_list = list(sentence.split(" "))
+for i in sentence_list:
+    print (i)
+    if i in topic:
+        counter  = counter+1
+        print(counter)
+       
